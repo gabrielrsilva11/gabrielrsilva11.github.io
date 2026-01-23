8a736a307e1bcd503e03ddbc26fffc85f77a6fa7
gabrielrsilva11 <gabrielrsilva11@gmail.com>
2026-01-23 14:26:15 +0000

    Deploying to gh-pages from @ gabrielrsilva11/gabrielrsilva11.github.io@09222d6f8ffdfb56b9e1f85bd12fdc7c0b4423f3 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -18,7 +18,7 @@ ninja.data = [{
           },
         },{id: "nav-cv",
           title: "cv",
-          description: ".",
+          description: "",
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
